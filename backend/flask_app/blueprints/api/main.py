19b0c25987b0ab68838626de740ce71931b85b25
Ayowale <haydexwisdom@gmail.com>
2023-11-28 13:22:31 +0100

    ADDED: Added review and registered course bluepruints

--- backend/flask_app/blueprints/api/main.py
+++ backend/flask_app/blueprints/api/main.py
@@ -10,3 +10,5 @@ api_blueprint = Blueprint("apis", __name__, url_prefix="/api")
 
 from backend.flask_app.blueprints.api import course
 from backend.flask_app.blueprints.api import user
+from backend.flask_app.blueprints.api import review
+from backend.flask_app.blueprints.api import registered_course
