808b17ca9cc78effd982375411c9cf0aca1607df
Ayowale <haydexwisdom@gmail.com>
2023-11-28 14:22:01 +0100

    FIXED: registered course route

--- backend/flask_app/blueprints/api/registered_course.py
+++ backend/flask_app/blueprints/api/registered_course.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 
 """
-Blueprint for the registered_courses route
+Blueprint for the registered_courses routes
 """
 
 from flask import jsonify, abort, request
 from backend.flask_app.blueprints.api.main import api_blueprint
 from backend.storage.tables.registered_courses import Registered
+from backend.storage.tables.course import Course
+from backend.storage.tables.user import User
 from backend import db, to_dict
 
 
 @api_blueprint.route("/registered", methods=["GET"])
 def registered_get():
+    """
+    Routes that get list of registered courses by a specified user
+    """
     user_id = request.args.get("user_id")
     if not user_id:
         abort(400, description="No user_id provided")
 
     r_objects = db.get_table(Registered)
-    if not r_objects:
+    if r_objects is None:
         abort(500, description="The database has encountered an error, please tr      y again")
 
     registered_c = []
@@ -27,7 +32,7 @@ def registered_get():
         if user_id == db_user_id:
             registered_c.append(r_dict)
 
-    if not registered_c:
+    if len(registered_c) == 0:
         abort(404, description="No registered courses found")
 
     res = jsonify({"registered_courses": registered_c})
@@ -37,6 +42,9 @@ def registered_get():
 
 @api_blueprint.route("/registered", methods=["POST"])
 def registered_post():
+    """
+    Route that adds to the list of registered courses of a user
+    """
     data = request.get_json()
     if not data:
         abort(400, description="Provided data not a JSON type")
@@ -51,6 +59,18 @@ def registered_post():
     d_course_id = data.get("course_id")
     d_user_id = data.get("user_id")
     d_course_title = data.get("course_title")
+
+    course_user = [
+            (d_course_id, Course),
+            (d_user_id, User)
+            ]
+    for param in course_user:
+        if len(db.get_row(param[1], param[0])) == 0:
+            des = "No {} linked to the provided id".format(param[1].__name__)
+            abort(400, description=des)
+        if db.get_row(param[1], param[0]) is None:
+            des = "Internal database problem, try again"
+            abort(500, description=des)
 
     new_registered_course = Registered(
             course_title=d_course_title,
