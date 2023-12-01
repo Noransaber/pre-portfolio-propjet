cad32ab9d1381b6d1112e768427ea34dfd06493b
Noransaber <noransaber685@gmail.com>
2023-12-01 22:52:03 +0200

    Changing the photos + making the button for all courses page

--- frontend/assets/script/main.js
+++ frontend/assets/script/main.js
@@ -1,27 +1,35 @@
-var user = localStorage.getItem("user-data");
+var user = localStorage.getItem('user-data');
 var JoinNow = document.querySelector('.joinNow');
 var login = document.querySelector('.login');
 var aboutBtn = document.querySelector('.about-btn');
-var logout = document.getElementById("logout");
+var logout = document.getElementById('logout');
+var showCourse = document.querySelector('.show-courses');
 
 function checkUserSign() {
-    if (user != null) {
-        if (JoinNow) {
-            JoinNow.style.display = "none";
-        }
-        if (login) {
-            login.style.display = "none";
-        }
-        if (logout) {
-            logout.style.display = "inline";;
-        }
+  if (user != null) {
+    if (JoinNow) {
+      JoinNow.style.display = 'none';
     }
+    if (login) {
+      login.style.display = 'none';
+    }
+    if (logout) {
+      logout.style.display = 'inline';
+    }
+  }
 }
-  
+
 logout.addEventListener('click', function () {
-    localStorage.removeItem("user-data");
-    // location.reload();
-    location.href = "index.html";
-})
-  
+  localStorage.removeItem('user-data');
+  // location.reload();
+  location.href = 'index.html';
+});
+
+if (showCourse) {
+  showCourse.addEventListener('click', function () {
+    // It render the signup.html page
+    location.href = 'allCourses.html';
+  });
+}
+
 checkUserSign();
