1cc61af9a50e8ebb6a59881bf7e7a319a0812510
Ayowale <haydexwisdom@gmail.com>
2023-12-02 14:05:45 +0100

    ADDED: Events on courses

--- frontend/assets/script/home.js
+++ frontend/assets/script/home.js
@@ -1,24 +1,38 @@
 var JoinNow = document.querySelector('.joinNow');
 var login = document.querySelector('.login');
 var aboutBtn = document.querySelector('.about-btn');
-var popularCourses = document.querySelector('.projcard-container') 
+var popularCourses = document.querySelector('.container') 
 var courses = []
 
 function getCourses() {
   fetch("http://localhost:5000/api/courses")
-    .then(function (data) {
+    .then(function(data) {
       if (!data.ok) {
-      	throw new Error('Error fetching courses!');	
+      	throw new Error('Error Fetching Courses!');	
       }
       return data.json();
     })
-    .then(function (data) {
-      courses = Object.values(data);
+    .then(function(data) {
+      courses = data.courses;
       console.log(courses);
+      popularCourses.innerHTML = courses.map((course)=>{
+	return `
+	<div class="square" onclick="courseClick('${course.id}')">
+          <img
+            src="${course.image_url}">
+            <div class="title">${course.title}</div>
+          <p class='desc'>${course.description}
+          </p>
+        </div>`
+      }).join("");
     })
     .catch(function(error) {
-      console.log(error);
+      popularCourses.innerHTML = `<p class='courseError'>Error Fetching Courses</p>`;
     })
+}
+
+function courseClick(course_id) {
+ console.log(course_id);
 }
 
 // Make an even, When we click on the button
